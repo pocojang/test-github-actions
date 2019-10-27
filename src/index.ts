9138fddf38ef6508ae016460d3026952d2a32b6e
devJang <devjang2016@gmail.com>
2019-10-27 23:56:39 +0900

    U

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 const test: string = 'hello world';
 const test2: string = 'hello world2';
+const test4: string = 'hello world2';
+const test6: string = 'hello world2';
 
-console.log(test);
-console.log(test);
+console.log(test6);
+console.log(test4);
 console.log(test2);
 console.log(test2);
